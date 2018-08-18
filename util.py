ee9172fbf2e6f63dcb850a133225ee34b448aefc
TsungYu HSIEH <i1007673219@gmail.com>
2018-08-18 13:00:57 +0800

    Fix repeated path join while load ckpt

--- util.py
+++ util.py
@@ -13,9 +13,10 @@ def load_ckpt(sess, saver, ckpt_dir='train'):
 
     '''Load checkpoint from the ckpt_dir and restore it to saver and sess, waiting 10 secs in the case of failure.'''
 
+    ckpt_dir = os.path.join(FLAGS.log_root, ckpt_dir)
+
     while True:
         try:
-            ckpt_dir = os.path.join(FLAGS.log_root, ckpt_dir)
             latest_filename = 'best.ckpt' if ckpt_dir == 'eval' else None
             ckpt_state = tf.train.get_checkpoint_state(ckpt_dir, latest_filename=latest_filename)
             saver.restore(sess, ckpt_state.model_checkpoint_path)
