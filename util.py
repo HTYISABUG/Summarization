bb2f3177af9ea0ee8e2909eb88d2bbcb95594dfd
TsungYu HSIEH <i1007673219@gmail.com>
2018-08-26 21:46:53 +0800

    Fix load_ckpt to restore best ckpt

--- util.py
+++ util.py
@@ -13,18 +13,18 @@ def load_ckpt(sess, saver, ckpt_dir='train'):
 
     '''Load checkpoint from the ckpt_dir and restore it to saver and sess, waiting 10 secs in the case of failure.'''
 
-    ckpt_dir = os.path.join(FLAGS.log_root, ckpt_dir)
+    _ckpt_dir = os.path.join(FLAGS.log_root, ckpt_dir)
 
     while True:
         try:
             latest_filename = 'best.ckpt' if ckpt_dir == 'eval' else None
-            ckpt_state = tf.train.get_checkpoint_state(ckpt_dir, latest_filename=latest_filename)
+            ckpt_state = tf.train.get_checkpoint_state(_ckpt_dir, latest_filename=latest_filename)
             saver.restore(sess, ckpt_state.model_checkpoint_path)
 
             tf.logging.info('Loading checkpoint %s', ckpt_state.model_checkpoint_path)
 
             return ckpt_state.model_checkpoint_path
         except:
-            tf.logging.info('Failed to load checkpoint from %s. Sleeping for %i secs...', ckpt_dir, 10)
+            tf.logging.info('Failed to load checkpoint from %s. Sleeping for %i secs...', _ckpt_dir, 10)
 
             time.sleep(10)
